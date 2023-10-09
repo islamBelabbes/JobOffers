1b08e65f056e969dd454184a97c13d36f16637cb
islamBelabbes <belabbesislam2@gmail.com>
2023-10-09 14:33:44 +0100

    fix Cors error

--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:3500",
-  "http://localhost:3000",
-  "https://jobblybuddy.vercel.app",
   "https://jobblybuddy.vercel.app/",
 ];
 
